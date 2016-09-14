f383f29726b6e3d922f95cac141251b49498efe3
William Thor Buckwell <thorbuckwell@gmail.com>
2016-09-14 17:16:57 -0400

    Still has errors I can't fix

---
+++ Engine/Weapons.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Weapons
+    {
+        private int id;
+        private String name;
+        private int cost;
+        private int damage;
+        private String damageType;
+
+        public Weapons(int id, String name, int cost, int damage, String damageType)
+        {
+            this.id = id;
+            this.name = name;
+            this.cost = cost;
+            this.damage = damage;
+            this.damageType = damageType;
+        }
+    }
+}
---
+++ Engine/World.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public static class World
+    {
+        public static readonly List<Monster> Monsters = new List<Monster>();
+        public static readonly List<Weapons> Weapons = new List<Weapons>();
+        public static readonly List<Item> Item = new List<Item>();
+
+    }
+}
