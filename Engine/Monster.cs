36c40a00f10a0c1cc0675b6e3fc3618bd2c87f96
sikderj3596 <sikderj3596@ATC15626>
2016-09-22 10:47:33 -0400

    Monster.cs copy

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -6,16 +6,16 @@ namespace Engine
     public class Monster
     {
         public int ID { get; set; }                         // To hold the ID number of the Monster
-        public string name { get; set; }                    // To hold the Monster's name
-        public int exp { get; set; }                        // To hold the amount of XP recieved for killing
-        public int gold { get; set; }                       // To hold amount of gold recieved from killing
-        public int ac { get; set; }                         // To hold how much armor the monster has
-        public int damage { get; set; }                     // To hold how much damage the monster does
-        public int baseAttack { get; set; }                 // To hold the plus or minus amount to ToHit
-        public string image { get; set; }                   // To hold the Monster's image string
-        public int currentHitPoints;
-        public int maxHitPoints;
-        public bool isDead;
+        public string Name { get; set; }                    // To hold the Monster's name
+        public int Exp { get; set; }                        // To hold the amount of XP recieved for killing
+        public int Gold { get; set; }                       // To hold amount of gold recieved from killing
+        public int AC { get; set; }                         // To hold how much armor the monster has
+        public int Damage { get; set; }                     // To hold how much damage the monster does
+        public int BaseAttack { get; set; }                 // To hold the plus or minus amount to ToHit
+        public string Image { get; set; }                   // To hold the Monster's image string
+        public int CurrentHitPoints { get; set; }
+        public int MaxHitPoints { get; set; }
+        public bool IsDead { get; set; }
 
         /**
          * This is the constructor for the Monster class. It accepts values to be assigned to the class variables
@@ -26,16 +26,31 @@ namespace Engine
             
         { 
             this.ID = id;
-            this.name = name;
-            this.exp = xp;
-            this.gold = gold;
-            this.ac = armor;
-            this.damage = dam;
-            this.baseAttack = ba;
-            this.image = image;
-            this.currentHitPoints = mobcurrentHitpoints;
-            this.maxHitPoints = mobmaximumHitpoints;
-            this.isDead = mobIsDead;
+            this.Name = name;
+            this.Exp = xp;
+            this.Gold = gold;
+            this.AC = armor;
+            this.Damage = dam;
+            this.BaseAttack = ba;
+            this.Image = image;
+            this.CurrentHitPoints = mobcurrentHitpoints;
+            this.MaxHitPoints = mobmaximumHitpoints;
+            this.IsDead = mobIsDead;
         }        
+
+        public Monster(Monster m)
+        {
+            this.ID = m.ID;
+            this.Name = m.Name;
+            this.Exp = m.Exp;
+            this.Gold = m.Gold;
+            this.AC = m.AC;
+            this.Damage = m.Damage;
+            this.BaseAttack = m.BaseAttack;
+            this.Image = m.Image;
+            this.CurrentHitPoints = m.CurrentHitPoints;
+            this.MaxHitPoints = m.MaxHitPoints;
+            this.IsDead = m.IsDead;
+        }
     }
 }
