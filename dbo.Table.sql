e7929ef2d27522133a1bace2ecbde082b41f7cca
thorbuckwel <thorbuckwell@gmail.com>
2016-10-20 10:57:36 -0400

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Weapons]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NULL, 
+    [Desc] NCHAR(10) NULL, 
+    [Damage] NCHAR(10) NULL, 
+    [Cost] NCHAR(10) NULL
+)
