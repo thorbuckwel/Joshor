bb1e006a5c0f882f2263a1f0e8e0f5335c369d18
buckwelw5455 <buckwelw5455@ATC15619>
2016-10-27 10:59:42 -0400

    add weapons

---
+++ Engine/Docs/dbo.Weapons.data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[Weapons] ([Id], [Name], [Desc], [Damage], [Cost], [Type]) VALUES (101, N'Club', N'A short stock', N'1d6', 100, N'Blunt')
+INSERT INTO [dbo].[Weapons] ([Id], [Name], [Desc], [Damage], [Cost], [Type]) VALUES (102, N'Dagger', N'a small piece of sharp metal', N'1d4', 50, N'Pierce')
+INSERT INTO [dbo].[Weapons] ([Id], [Name], [Desc], [Damage], [Cost], [Type]) VALUES (103, N'Falchion', N'Sword', N'1d8', 200, N'Slash')
